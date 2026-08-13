359efdd1615b82615f62a7f75729dfc7c456a5e8
bloomflower2002@gmail.com <bloomflower2002@gmail.com>
2026-08-13 21:05:13 +0300

    fix: fix the issue of the footer not being displayed on the signin page

--- Frontend/components/Layout.tsx
+++ Frontend/components/Layout.tsx
@@ -12,6 +12,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const pathname = usePathname();
   const shouldShowNavbar = pathname !== '/signin' && pathname !== '/signin/';
+  const shouldShowFooter = pathname !== '/signin' && pathname !== '/signin/';
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -19,7 +20,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <main className={`flex-1 ${shouldShowNavbar ? 'pt-[70px] md:pt-[70px]' : 'pt-0'}`}>
         {children}
       </main>
-      <Footer />
+      {shouldShowFooter && <Footer />}
     </div>
   );
 };
